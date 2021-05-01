1d5e9b3f049b2bae5fee144edc606e1a76f3a7b0
Sankar Kumar <Sankar Kumar@DESKTOP-OG8C1AF>
2021-05-01 19:02:12 +0530

    Added comments

--- src/Practice/RotateCheck.java
+++ src/Practice/RotateCheck.java
@@ -2,6 +2,7 @@ package Practice;
 
 public class RotateCheck {
 	public static void main(String args[]) {
+		//we have to add the second str twice to compare the first string
 		String s1="race";
 		String s2="cera";
 		String s3=s2+s2;
