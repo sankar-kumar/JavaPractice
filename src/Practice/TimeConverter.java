da8b19ffaed5cc4af575c310dba2fed7e6b42226
Sankar Kumar <Sankar Kumar@DESKTOP-OG8C1AF>
2021-05-23 13:15:09 +0530

    Time converter from 12 hours to 24 hours time zone.

--- src/Practice/TimeConverter.java
+++ src/Practice/TimeConverter.java
@@ -4,13 +4,17 @@ import java.util.Scanner;
 
 public class TimeConverter {
 	public static void main(String[] args) {
-		
-		Scanner sc=new Scanner(System.in);
-		String time=sc.next();
-		String hh=time.substring(0, 2);
-		String zn=time.substring(8);
+
+		// Input get 12:00:00AM in this format. Convert time from 12 hour to 24 hour
+		// time Zone.
+		// remember 12 AM is 00 in 24 hr. 12PM is 12 only.hope below code will be helpfull to understand better.
+
+		Scanner sc = new Scanner(System.in);
+		String time = sc.next();
+		String hh = time.substring(0, 2);
+		String zn = time.substring(8);
 		int H = Integer.parseInt(hh);
-		
+
 		if (zn.equals("AM")) {
 			if (H == 12)
 				hh = "00";
@@ -20,17 +24,17 @@ public class TimeConverter {
 				}
 			}
 		}
-		
-		else if(zn.equals("PM")){
-			if(H==12)
-				hh="12";
+
+		else if (zn.equals("PM")) {
+			if (H == 12)
+				hh = "12";
 			else {
-				H=H+12;
-				hh=Integer.toString(H);
+				H = H + 12;
+				hh = Integer.toString(H);
 			}
 		}
-		
-		System.out.println(hh+time.substring(2,8));
+
+		System.out.println(hh + time.substring(2, 8));
 	}
 
 }
