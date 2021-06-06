dad6157e9ff8d07ab57919de755c9260e7896593
sankar-kumar <sankarkumar2709@gmail.com>
2021-06-06 19:01:11 +0530

    Worked on matrix diagonal difference.

--- src/Practice/MatrixDiagonalDiff.java
+++ src/Practice/MatrixDiagonalDiff.java
@@ -6,21 +6,30 @@ public class MatrixDiagonalDiff {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//		find difference between left diagonal and right diagonal
+		// find difference between left diagonal and right diagonal
+		// Need to work on this!!!
 		Scanner sc = new Scanner(System.in);
-		int len = 3, sum = 0;
-		int ar[] = new int[len];
+		int len = 3, d1 = 0, d2 = 0;
+		int ar[][] = new int[len][len];
 		for (int i = 0; i < len; i++) {
 			for (int j = 0; j < len; j++) {
-				ar[i] = sc.nextInt();
-//				System.out.print("* ");
-				if (j % 2 == 0) {
-					sum = sum + ar[i];
-				}
+				ar[i][j] = sc.nextInt();
+				// System.out.print(ar[i]);
+				if (i == j)
+					d1 += ar[i][j];
 			}
 		}
-		System.out.println(sum);
 
+		for (int k = 0; k < len; k++) {
+			for (int l = len - k - 1; l >= 0; l--) {
+				d2 += ar[k][l];
+				break;
+			}
+		}
+		sc.close();
+		
+		System.out.print(Math.abs(d1-d2));
+//		System.out.print(d2);
 	}
 
 }
