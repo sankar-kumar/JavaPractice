1544fed08342a40f977217d7b4ea154be32ea77a
sankar-kumar <sankarkumar2709@gmail.com>
2021-10-17 10:35:59 +0530

    Some work just for more clarity.. But want to go through again.

---
+++ src/DataStructurePractice/Arrays.java
@@ -0,0 +1,15 @@
+package DataStructurePractice;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x[] = { 1, 2, 4, 5, 6 };
+		x[0] = 99;
+		for (int i = 0; i < x.length; i++) {
+			System.out.println(x[i]);
+		}
+
+	}
+
+}
--- src/Practice/ArrayBasics.java
+++ src/Practice/ArrayBasics.java
@@ -1,5 +1,9 @@
 package Practice;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class ArrayBasics {
 
 	public static void main(String[] args) {
@@ -8,7 +12,12 @@ public class ArrayBasics {
 		// 2D array means there is multiple rows and multiple columns
 		// Just for learning.. Here i implemented jagged array
 		int i[][] = { { 12, 1, 1, 4 }, { 1, 4, 99 }, { 55, 88, 33, 44, 66 } };
-
+		int p[] = { 4, 9, 6, 8, 6 };
+		List<Integer> intList=new ArrayList<>();
+		intList.add(50);
+		for (int aa = 0; aa < p.length; aa++) {
+			System.out.println(p[aa]);
+		}
 		for (int j = 0; j < i.length; j++) {
 			for (int k = 0; k < i[j].length; k++) {
 				System.out.print(i[j][k] + " ");
