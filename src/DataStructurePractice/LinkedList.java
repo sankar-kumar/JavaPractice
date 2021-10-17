865dff6aa830b899125b83c528e500114d5a2a7e
sankar-kumar <sankarkumar2709@gmail.com>
2021-10-17 17:08:39 +0530

    Finally copied code from Telusko Learnings,Thanks Navin.

--- src/DataStructurePractice/LinkedList.java
+++ src/DataStructurePractice/LinkedList.java
@@ -20,6 +20,47 @@ public class LinkedList {
 		}
 	}
 
+	public void insertAtStart(int data) {
+		Node node = new Node();
+		node.data = data;
+		node.next = null;
+		node.next = head;
+		head = node;
+	}
+
+	public void insertAt(int index, int data) {
+		Node node = new Node();
+		node.data = data;
+		node.next = null;
+
+		if (index == 0) {
+			insertAtStart(data);
+		} else {
+			Node n = head;
+			for (int i = 0; i < index - 1; i++) {
+				n = n.next;
+			}
+			node.next = n.next;
+			n.next = node;
+		}
+	}
+
+	public void deleteAt(int index) {
+		if (index == 0) {
+			head = head.next;
+		} else {
+			Node n = head;
+			Node n1 = null;
+			for (int i = 0; i < index - 1; i++) {
+				n = n.next;
+			}
+			n1 = n.next;
+			n.next = n1.next;
+			// System.out.println("n1 " + n1.data);
+			n1 = null;
+		}
+	}
+
 	public void show() {
 		Node node = head;
 		while (node.next != null) {
--- src/DataStructurePractice/LinkedListRunner.java
+++ src/DataStructurePractice/LinkedListRunner.java
@@ -6,6 +6,10 @@ public class LinkedListRunner {
 		list.insert(55);
 		list.insert(78);
 		list.insert(95);
+		list.insertAt(2,99);
+		list.insertAtStart(100);
+		list.insertAt(0, 130);
+		list.deleteAt(2);
 		
 		list.show();
 	}
